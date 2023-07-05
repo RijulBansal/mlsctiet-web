810f5ebe67ca14ea763037695f74a2d34909b45a
Debaditya <debadityabarman12@gmail.com>
2023-07-05 11:09:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import AboutUs from "./Pages/AboutUs";
+import Alumnis from "./Pages/Alumni";
+// import Community from "./Pages/Community";
+import Events from "./Pages/Events";
+import Gallery from "./Pages/Gallery";
+import LandingPage from "./Pages/LandingPage";
+import OurTeam from "./Pages/OurTeam";
+import Error from "./Pages/Errorpage/Errorpage";
+import Sponsors from "./Pages/Sponsors";
+import Navbar from "./Components/Navbar/Navbar";
+import PolaroidGrid from "./Pages/Alumni/PolaroidGrid";
+import Main from "./Pages/Alumni/Main";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Carousels from "./Pages/Alumni/Components/Carousels";
+import Polaroid from "./Pages/Alumni/Components/Polaroid";
+// import Projects from "./Pages/Projects";
+// import Project1 from "./Pages/Projects/Components/Project1/Project1";
+// import Project2 from "./Pages/Projects/Components/Project2/Project2";
+// import Project3 from "./Pages/Projects/Components/Project3/Project3";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutUs />} />
+        {/* <Route path="/community" element={<Community />} /> */}
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/team" element={<OurTeam />} />
+        <Route path="/sponsors" element={<Sponsors />} />
+        <Route path="/alumni" element={<Alumnis />}>
+          <Route index element={<Main />} />
+          <Route path=":year" element={<PolaroidGrid/>} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Pages/Community/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "./Components/Main.jsx";
+
+const Community = () => {
+  return (
+    <>
+      <Main></Main>
+    </>
+  );
+};
+
+export default Community;
