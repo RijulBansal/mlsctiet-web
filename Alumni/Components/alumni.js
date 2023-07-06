9243d7f115e7eebcd4e62f4731974d2f197e8836
Rijul Bansal <84855217+RijulBansal@users.noreply.github.com>
2023-07-07 01:54:27 +0530

    Add files via upload

---
+++ Alumni/Components/alumni.js
@@ -0,0 +1,199 @@
+const Alumni = [
+  {
+    id: 1,
+    year: 2022,
+    image:
+      "https://media.discordapp.net/attachments/860744459406147618/998936557648085024/alumni.jpg?width=662&height=662",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+
+  {
+    id: 2,
+    year: 2022,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 3,
+    year: 2022,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+
+  {
+    id: 4,
+    year: 2022,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+
+  {
+    id: 5,
+    year: 2022,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 6,
+    year: 2021,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 7,
+    year: 2021,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 8,
+    year: 2021,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 9,
+    year: 2021,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 10,
+    year: 2021,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 11,
+    year: 2020,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 12,
+    year: 2020,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 13,
+    year: 2020,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 14,
+    year: 2020,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 15,
+    year: 2020,
+    image:
+      "https://media.discordapp.net/attachments/860744459406147618/998936557648085024/alumni.jpg?width=662&height=662",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 16,
+    year: 2019,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 17,
+    year: 2019,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 18,
+    year: 2019,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 19,
+    year: 2019,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 20,
+    year: 2018,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 21,
+    year: 2018,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 22,
+    year: 2018,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 23,
+    year: 2018,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+  {
+    id: 24,
+    year: 2018,
+    image:
+      "https://res.cloudinary.com/Yashvi/image/upload/v1658234631/alumni_cmmwys.jpg",
+    name: "Rijul",
+    linkedIn: "https://www.linkedin.com/",
+  },
+];
+
+export default Alumni;
